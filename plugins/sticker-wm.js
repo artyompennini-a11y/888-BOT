829b4bda2d92a27e6d87dc82014eb848c15eec87
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-10 17:26:02 +0200

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,25 +1,38 @@
-//Plugin by Gab, Lucifero & 333 staff
-
 import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw 'rispondi a uno sticker'
-let stiker = false
-try {
-let [packname, ...author] = text.split('|')
-author = (author || []).join('|')
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw 'rispondi a uno sticker'
-let img = await m.quoted.download()
-if (!img) throw 'rispondi a uno sticker'
-stiker = await addExif(img, packname || '', author || '')
-} catch (e) {
-console.error(e)
-if (Buffer.isBuffer(e)) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw 'errore'
-}}
-handler.help = ['wm <packname>|<author>']
-handler.tags = ['sticker']
-handler.command = /^robar|wm$/i
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!m.quoted) return m.reply(`『 ✧ 』 - \`Rispondi allo sticker che vuoi personalizzare\``)
+  
+  let stiker = false
+  try {
+    if (!text) {
+      let name = conn.getName(m.sender)
+      text = `${name}|𝟴𝟴𝟴 𝗕𝗢𝗧`
+    }
+    
+    let [packname, ...author] = text.split('|')
+    author = (author || []).join('|')
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) return m.reply(`『 ✧ 』- \`Rispondi a uno sticker\``)
+    
+    let img = await m.quoted.download()
+    if (!img) return m.reply(`${global.errore}`)
+    stiker = await addExif(img, packname || '', author || '')
+    
+  } catch (e) {
+    console.error('Errore in sticker-wm:', e)
+    if (Buffer.isBuffer(e)) stiker = e
+  } finally {
+    if (stiker) {
+      await conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
+    } else {
+      m.reply(`${global.errore}`)
+    }
+  }
+}
+
+handler.help = ['wm']
+handler.tags = ['sticker', 'strumenti']
+handler.command = ['take', 'wm']
+handler.register = false
 export default handler

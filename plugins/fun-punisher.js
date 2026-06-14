380da792c3fedf62c9f1c26f662ab40f496c8e58
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-14 12:49:58 +0200

    Update help command from 'punisher' to 'xxxx'

--- plugins/fun-punisher.js
+++ plugins/fun-punisher.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 };
 
-handler.help = ['punisher'];
+handler.help = ['xxxx'];
 handler.tags = ['giochi'];
 
 // Questa regex rileva "punisher" ovunque nel messaggio, ignorando maiuscole/minuscole

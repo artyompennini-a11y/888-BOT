96131ad4f8da3a6cce35681ae7f5d5878181cd72
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-16 16:14:15 +0200

    Update gp-aperto.js

--- plugins/gp-aperto.js
+++ plugins/gp-aperto.js
@@ -1,44 +1,49 @@
 let handler = async (m, { conn }) => {
-  
-  
-  await conn.groupSettingUpdate(m.chat, 'not_announcement')
+  
+  
+  await conn.sendMessage(m.chat, { react: { text: '🔓', key: m.key } })
 
-  const message = "« 🔓 »  *L'ARENA È APERTA*\n\n> *Il silenzio è rotto. La parola torna al popolo. Esprimetevi con saggezza.*"
+ 
+  await conn.groupSettingUpdate(m.chat, 'not_announcement')
 
-  const buttons = [
-    {
-      buttonId: '.chiuso',
-      buttonText: { displayText: '🔒 Chiudi Gruppo' },
-      type: 1
-    }
-  ]
+  const message = "« 🔓 »  *L'ARENA È APERTA*\n\n> *Il silenzio è rotto. La parola torna al popolo. Esprimetevi con saggezza.*"
 
-  const buttonMessage = {
-    text: message,
-    footer: '⚙️ Pannello Rapido Amministrazione',
-    buttons: buttons,
-    headerType: 1,
-    contextInfo: {
-      externalAdReply: {
-        title: '〔 ACCESS GRANTED 〕',
-        body: 'Gestionale Sistema 𝟴𝟴𝟴 𝗕𝗢𝗧',
-        thumbnailUrl: 'https://images.unsplash.com/photo-1614064641938-3bbee52942c7?q=80&w=500', 
-        sourceUrl: '',
-        mediaType: 1,
-        renderLargerThumbnail: false
-      },
-      forwardingScore: 999,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363427251015414@newsletter',
-        newsletterName: `✦ ${global.db?.data?.nomedelbot || '𝟴𝟴𝟴 𝗕𝗢𝗧'} ✦`,
-        serverMessageId: 143
-      }
-    }
-  }
+  
+  const buttons = [
+    {
+      buttonId: '.chiuso',
+      buttonText: { displayText: '🔒 Chiudi Gruppo' },
+      type: 1
+    }
+  ]
 
-  
-  await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+
+  const buttonMessage = {
+    text: message,
+    footer: '⚙️ Pannello Rapido Amministrazione',
+    buttons: buttons,
+    headerType: 1,
+    contextInfo: {
+      externalAdReply: {
+        title: '〔 ACCESS GRANTED 〕',
+        body: 'Gestionale Sistema 𝟴𝟴𝟴 𝗕𝗢𝗧',
+        thumbnailUrl: 'https://images.unsplash.com/photo-1614064641938-3bbee52942c7?q=80&w=500', 
+        sourceUrl: '',
+        mediaType: 1,
+        renderLargerThumbnail: false
+      },
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        
+        newsletterJid: '120363427251015414@newsletter',
+        newsletterName: `✦ ${global.db?.data?.nomedelbot || '𝟴𝟴𝟴 𝗕𝗢𝗧'} ✦`,
+        serverMessageId: 143
+      }
+    }
+  }
+
+  await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
 
 handler.help = ['apri']
@@ -47,4 +52,4 @@ handler.command = ['apri']
 handler.admin = true
 handler.botAdmin = true
 
-export default handler
+export default handler         

18c250505378944958439ff18b8d496775b5972b
THE PUNISHER <artyom.pennini@gmail.com>
2026-06-23 23:02:43 +0200

    Update gp-rimuovi.js

--- plugins/gp-rimuovi.js
+++ plugins/gp-rimuovi.js
@@ -48,10 +48,11 @@ var handler = async (m, { conn, participants }) => {
     }
 }
 
-handler.help = ['rimuovi']
+handler.help = ['kick']
 handler.tags = ['gruppo']
-handler.command = /^(kick|rimuovi|paki|ban|abdul)$/i 
+handler.command = /^(kick|paki|ban|abdul)$/i 
 handler.group = true
+handler.mods = true
 handler.admin = true
 handler.botAdmin = true
 

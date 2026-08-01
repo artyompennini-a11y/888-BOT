bd32b853689222d292413ac17fcc4ea25968d368
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-01 19:47:07 +0200

    Update RPG-dadi_menu.js

--- plugins/RPG-dadi_menu.js
+++ plugins/RPG-dadi_menu.js
@@ -7,11 +7,11 @@ let handler = async (m, { conn }) => {
 
   await conn.sendMessage(m.chat, {
     text:
-`╔═🎲 𝐃𝐀𝐃𝐈 ═╗
+`╭━━━〔🎲 𝐃𝐀𝐃𝐈 〕━━━┈
 ┃ 💰 Portafoglio: *${money}€*
 ┃
 ┃ Scegli la puntata
-╚══════╝`,
+╰━━━━━━━━━━━━━━━━━━┈`,
     buttons: [
       { buttonId: bet(100), buttonText: { displayText: "100€" }, type: 1 },
       { buttonId: bet(200), buttonText: { displayText: "200€" }, type: 1 },

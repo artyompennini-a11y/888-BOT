e5bda5b39ecc9ba1c4380af39fbcc80bd84a468a
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-01 18:22:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,19 +11,19 @@ import NodeCache from 'node-cache'
 const pkg = JSON.parse(await fs.promises.readFile(new URL('./package.json', import.meta.url), 'utf-8'))
 const moduleCache = new NodeCache({ stdTTL: 300 });
 
-// Configurazione OpenRouter Key
-global.openrouterApiKey = 'sk-or-v1-29c...3ea'
-
+	
 global.gab = ['573117824583','xxxx','xxxx',]
 global.owner = [
-  ['xxxx', 'xxxx', true],
-  ['xxxx', 'xxxxx', true],
-  ['xxxx', 'xxxx', true],
-  ['573117824583', 'Punisher3', true],
-  ['xxxx', 'xxxx', true],
-  ['212785655331', 'Ghost', true],
-  ['xxxx', 'xxxx', true],	
+  ['xxxx', 'xxxx', true],
+  ['xxxx', 'xxxxx', true],
+  ['xxxx', 'xxxx', true],
+  ['573117824583', 'Punisher3', true],
+  ['xxxx', 'xxxx', true],
+  ['212785655331', 'Ghost', true],
+  ['xxxx', 'xxxx', true],	
+ 
 ]
+
 
 global.nomepack = '𝟴𝟴𝟴 𝗕𝗢𝗧'
 global.nomebot = '𝟴𝟴𝟴 𝗕𝗢𝗧'
@@ -34,9 +34,11 @@ global.testobot = `𝟴𝟴𝟴 𝗕𝗢𝗧`
 global.versione = pkg.version
 global.errore = '⚠️ *Errore inatteso!* Usa il comando `.ticket` per avvisare gli owner.'
 
+
 global.repobot = 'https//wa.me/393206032199'
 global.canale = 'https://whatsapp.com/channel/0029VauhQviCsU9Ibrwlkb0h'
-global.gruppo = 'https://chat.whatsapp.com/KqBeKHgrc53BNdvuPTKLTL' 
+global.gruppo = 'https://chat.whatsapp.com/KqBeKHgrc53BNdvuPTKLTL' 
+
 
 global.cheerio = cheerio
 global.fs = fs
@@ -44,35 +46,37 @@ global.fetch = fetch
 global.axios = axios
 global.moment = moment
 
-global.APIKeys = { 
-    spotifyclientid: '333',
-    spotifysecret: '333',
-    browserless: '333',
-    screenshotone: '333',
-    screenshotone_default: '333',
-    tmdb: '333',
-    gemini:'333',
-    ocrspace: '333',
-    assemblyai: '333',
-    google: '333',
-    googlex: '333',
-    googleCX: '333',
-    genius: '333',
-    unsplash: '333',
-    removebg: 'FEx4CYmYN1QRQWD1mbZp87jV',
-    openrouter: global.openrouterApiKey,
-    lastfm: '36f859a1fc4121e7f0e931806507d5f9',
+
+global.APIKeys = { 
+    spotifyclientid: '333',
+    spotifysecret: '333',
+    browserless: '333',
+    screenshotone: '333',
+    screenshotone_default: '333',
+    tmdb: '333',
+    gemini:'333',
+    ocrspace: '333',
+    assemblyai: '333',
+    google: '333',
+    googlex: '333',
+    googleCX: '333',
+    genius: '333',
+    unsplash: '333',
+    removebg: 'FEx4CYmYN1QRQWD1mbZp87jV',
+    openrouter: '333',
+    lastfm: '36f859a1fc4121e7f0e931806507d5f9',
 }
+
 
 let filePath = fileURLToPath(import.meta.url)
 let fileUrl = pathToFileURL(filePath).href
 const reloadConfig = async () => {
-  const cached = moduleCache.get(fileUrl);
-  if (cached) return cached;
-  unwatchFile(filePath)
-  console.log(chalk.bgHex('#ff0000')(chalk.white.bold("File: 'config.js' Aggiornato")))
-  const module = await import(`${fileUrl}?update=${Date.now()}`)
-  moduleCache.set(fileUrl, module, { ttl: 300 });
-  return module;
+  const cached = moduleCache.get(fileUrl);
+  if (cached) return cached;
+  unwatchFile(filePath)
+  console.log(chalk.bgHex('#ff0000')(chalk.white.bold("File: 'config.js' Aggiornato")))
+  const module = await import(`${fileUrl}?update=${Date.now()}`)
+  moduleCache.set(fileUrl, module, { ttl: 300 });
+  return module;
 }
-watchFile(filePath, reloadConfig)
+watchFile(filePath, reloadConfig)                   

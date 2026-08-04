70c067caee5e63492ae2d0e0ca3dcefa1c8f7448
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-04 10:54:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ const moduleCache = new NodeCache({ stdTTL: 300 });
 global.gab = ['573117824583','639753555926','xxxx',]
 global.owner = [
   ['639753555926', 'megamind', true],
-  ['xxxx', 'xxxxx', true],
-  ['213790413542', 'Momo', true],
+  ['xxxx', 'xxxx', true],
+  ['xxxx', 'xxxx', true],
   ['573117824583', 'Punisher3', true],
-  ['14384448399', '888bot', true],
+  ['573180770909', '888bot', true],
   ['212785655331', 'Ghost', true],
   ['212656627725', 'Malphas', true],	
  

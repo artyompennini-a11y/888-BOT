83b84068662f89f85069e60cd41eb4fe2c047e81
THE PUNISHER <artyom.pennini@gmail.com>
2026-08-08 12:14:09 +0200

    Modify group links and author names in OWNER_spam.js
    Updated group links and plugin author names in OWNER_spam.js.

--- plugins/OWNER_spam.js
+++ plugins/OWNER_spam.js
@@ -1,12 +1,20 @@
-//Plugin by punisher, elixir & 888 staff
+// Plugin by Elixir, Punisher & 888 staff
 
+const manually = `𝐆𝐑𝐔𝐏𝐏𝐈 𝐔𝐅𝐅𝐈𝐂𝐈𝐀𝐋𝐈:
 
-
-const manually = `entrate tutti qua 
-https://chat.whatsapp.com/GvNGkCEGKEy1ug1ZVOsXJW
-
-e stiamo cambiando gruppo, tutti qua 
-https://chat.whatsapp.com/C4OojVoyFbeCSxPSolZvrG`
+╭───⭓
+│ 🗨️ 𝗚𝗿𝘂𝗽𝗽𝗼 𝟭
+│ https://chat.whatsapp.com/Dxfu8kYcAhaIVZIVdKwGRc
+│
+│ 🗨️ 𝗚𝗿𝘂𝗽𝗽𝗼 𝟮
+│ https://chat.whatsapp.com/KALEdOncDa7Imw7NTLwYyw
+│
+│ 🗨️ 𝗚𝗿𝘂𝗽𝗽𝗼 𝟯
+│ https://chat.whatsapp.com/DjDBrPXWZLOCAoHMA1oNND
+│
+│ 🗨️ 𝗚𝗿𝘂𝗽𝗽𝗼 𝟰
+│ https://chat.whatsapp.com/BTl1RY8cqR4BUeRQIyoAXD
+╰───⭓`
 import { generateWAMessageFromContent } from '@realvare/baileys'
 const handler = async (m, { args, text }) => {
 if (parseInt(args[1])) return m.reply(`Inserisci prima la quantità di messaggi da inviare e poi il testo`)

2f8bad878e04ca0386833299007a138b899fca2b
TliedDeveloperMode <tlied@outlook.it>
2026-09-01 13:07:02 +0200

    fix tette tag

--- plugins/fun-tette.js
+++ plugins/fun-tette.js
@@ -5,34 +5,35 @@
 
 
 let handler = async (m, { conn, command, text }) => {
-let target = m.mentionedJid?.[0] 
-  || m.quoted?.sender 
-  || m.sender
+    
+    let target = m.mentionedJid?.[0] 
+                || (args[0]?.includes('@') ? args[0].replace('@', '') + '@s.whatsapp.net' : null)
+                || m.sender
 
-let number = target.split("@")[0]
+    let number = target.split("@")[0]
 
-    let boobsSizes = ['prima', 'seconda', 'terza', 'quarta', 'quinta', 'sesta', 'settima', 'ottava', 'nona', 'decima'];
-
-
-    let size = pickRandom(boobsSizes);
+        let boobsSizes = ['prima', 'seconda', 'terza', 'quarta', 'quinta', 'sesta', 'settima', 'ottava', 'nona', 'decima'];
 
 
-    let boobs = `*🍒 𝐂𝐀𝐋𝐂𝐎𝐋𝐈𝐀𝐌𝐎 𝐋𝐄 𝐓𝐔𝐄 𝐁𝐎𝐎𝐁𝐒 🍒*\n
-━━━━━━━━━━━━━━━━
-*@${number}* 𝐡𝐚 𝐮𝐧𝐚 *${size}*
-━━━━━━━━━━━━━━━━\n> 𝟴𝟴𝟴 𝗕𝗢𝗧`.trim()
+        let size = pickRandom(boobsSizes);
 
 
-    m.reply(boobs, null, { mentions: conn.parseMention(boobs) })
-}
+        let boobs = `*🍒 𝐂𝐀𝐋𝐂𝐎𝐋𝐈𝐀𝐌𝐎 𝐋𝐄 𝐓𝐔𝐄 𝐁𝐎𝐎𝐁𝐒 🍒*\n
+    ━━━━━━━━━━━━━━━━
+    *@${number}* ha una *${size}*
+    ━━━━━━━━━━━━━━━━\n> *888 BOT*`.trim()
 
 
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)];
-}
+        m.reply(boobs, null, { mentions: conn.parseMention(boobs) })
+    }
+
+
+    function pickRandom(list) {
+        return list[Math.floor(Math.random() * list.length)];
+    }
 
 handler.help = ['𝐭𝐞𝐭𝐭𝐞 @𝐭𝐚𝐠']
 handler.tags = ['fun']
-handler.command = /^(tette)$/i
+handler.command = ['tette']
 
 export default handler;

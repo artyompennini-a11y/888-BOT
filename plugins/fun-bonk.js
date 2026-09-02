873e2f90ff6c799cee3c7c130095925b4b966858
THE PUNISHER <artyom.pennini@gmail.com>
2026-09-02 09:40:32 +0200

    Refactor fun-bonk.js to use consistent imports

--- plugins/fun-bonk.js
+++ plugins/fun-bonk.js
@@ -1,31 +1,32 @@
-//Plugin by Gab, Lucifero & 333 staff
-
-import jimp from 'jimp'
-import fs from 'fs'
+import Jimp from 'jimp';
+import fs from 'fs';
 
 let handler = async (m, { conn }) => {
 
-    let who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender
+    let who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender;
 
-    let img = await jimp.read('./icone/bonk.png')
+    // Leggi immagine bonk
+    const img = await Jimp.read('./icone/bonk.png');
 
-    let avatar
+    // Avatar utente
+    let avatar;
     try {
-        avatar = await jimp.read(await conn.profilePictureUrl(who, 'image'))
+        const url = await conn.profilePictureUrl(who, 'image');
+        avatar = await Jimp.read(url);
     } catch {
-        avatar = await jimp.read('./icone/default.png')
+        avatar = await Jimp.read('./icone/default.png');
     }
 
-    avatar.resize(128, 128)
+    avatar.resize(128, 128);
 
-    let bonk = await img
+    const bonk = await img
         .composite(avatar, 120, 90)
-        .getBufferAsync('image/png')
+        .getBufferAsync('image/png');
 
-    await conn.sendMessage(m.chat, { image: bonk }, { quoted: m })
-}
+    await conn.sendMessage(m.chat, { image: bonk }, { quoted: m });
+};
 
-handler.command = /^(bonk)$/i
-handler.group = true
+handler.command = /^(bonk)$/i;
+handler.group = true;
 
-export default handler
+export default handler;

a184284e5cfd5bbf099e83cc43e224ddb5dd822d
AxtralWiZaRd <axtralwizard06@gmail.com>
2026-09-16 21:13:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -43,6 +43,7 @@ global.owner = normalizeOwnerList([
   ['573180770909', '888bot', true],
   ['212785655331', 'Ghost', true],
   ['447785114563', 'Dado', true],
+  ['17577575541', 'Axtral', true],
   ['393784409415', 'Bot', true],
 ])
 

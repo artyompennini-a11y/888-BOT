a33a6e875b50540c1506b1d7805b9f501f9be800
THE PUNISHER <artyom.pennini@gmail.com>
2026-09-16 17:00:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -33,13 +33,13 @@ function normalizeOwnerList(list) {
    LISTE GLOBALI
    ============================================================ */
 
-global.gab = ['573117824583','393297014539','xxxx']
+global.gab = ['972559226174','393297014539','xxxx']
 
 global.owner = normalizeOwnerList([
   ['393297014539', 'elixir', true],
   ['393331663641', 'Manu', true],              
   ['972552671306', 'manux', true],
-  ['573117824583', 'Punisher', true],
+  ['972559226174', 'Punisher', true],
   ['573180770909', '888bot', true],
   ['212785655331', 'Ghost', true],
   ['447785114563', 'Dado', true],

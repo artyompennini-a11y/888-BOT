76367c306e910329196091c3734ba4d574f460c3
THE PUNISHER <artyom.pennini@gmail.com>
2026-09-17 02:54:46 +0200

    Update owner-restart.js

--- plugins/owner/owner-restart.js
+++ plugins/owner/owner-restart.js
@@ -1,16 +1,15 @@
-//Plugin by Gab, Lucifero & 888 staff
-
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const handler = async (m, { conn, isOwner }) => {
     const userId = m.sender;
     const groupId = m.chat;
 
-    if (!isOwner) return m.reply("❌ 𝐒𝐨𝐥𝐨 𝐢𝐥 𝐩𝐫𝐨𝐩𝐫𝐢𝐞𝐭𝐚𝐫𝐢𝐨 𝐩𝐮𝐨̀ 𝐮𝐬𝐚𝐫𝐞 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐦𝐚𝐧𝐝𝐨.")
+    if (!isOwner) 
+        return m.reply("❌ Solo il proprietario può usare questo comando.");
 
     try {
         const { key } = await conn.sendMessage(m.chat, {
-            text: `╭─────────╮\n┃🔄 𝐑𝐈𝐀𝐕𝐕𝐈𝐎 𝐈𝐍 𝐂𝐎𝐑𝐒𝐎...\n┃⏱️ 𝐀𝐭𝐭𝐞𝐧𝐝𝐢 𝐪𝐮𝐚𝐥𝐜𝐡𝐞 𝐬𝐞𝐜𝐨𝐧𝐝𝐨.\n╰─────────╯\n> 𝟴𝟴𝟴 𝗕𝗢𝗧 𝐫𝐞𝐬𝐭𝐚𝐫𝐭`
+            text: `🔄 *Riavvio in corso...*\n⏱️ Attendi qualche secondo.\n\n> 888 BOT restart`
         }, { quoted: m });
 
         await delay(1000);
@@ -23,14 +22,14 @@ const handler = async (m, { conn, isOwner }) => {
         await delay(1000);
 
         await conn.sendMessage(m.chat, {
-            text: `╭─────────╮\n┃⚙️ 𝐀𝐯𝐯𝐢𝐨 𝐩𝐫𝐨𝐜𝐞𝐝𝐮𝐫𝐚...\n┃🔃 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 𝐦𝐨𝐝𝐮𝐥𝐢 𝐢𝐧 𝐜𝐨𝐫𝐬𝐨.\n╰─────────╯\n> 𝟴𝟴𝟴 𝗕𝗢𝗧 𝐫𝐞𝐬𝐭𝐚𝐫𝐭`,
+            text: `⚙️ *Avvio procedura...*\n🔃 Caricamento moduli in corso.\n\n> 888 BOT restart`,
             edit: key
         });
 
         await delay(1000);
 
         await conn.sendMessage(m.chat, {
-            text: `╭─────────╮\n┃✅ 𝐑𝐈𝐀𝐕𝐕𝐈𝐎 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐀𝐓𝐎!\n┃🟢 𝐁𝐨𝐭 𝐨𝐧𝐥𝐢𝐧𝐞 𝐚 𝐛𝐫𝐞𝐯𝐞.\n╰─────────╯\n> 𝟴𝟴𝟴 𝗕𝗢𝗧 𝐫𝐞𝐬𝐭𝐚𝐫𝐭`,
+            text: `✅ *Riavvio completato!*\n🟢 Bot online a breve.\n\n> 888 BOT restart`,
             edit: key
         });
 
@@ -39,7 +38,7 @@ const handler = async (m, { conn, isOwner }) => {
         process.exit(42);
 
     } catch (error) {
-        m.reply(`❌ 𝐄𝐫𝐫𝐨𝐫𝐞 𝐝𝐮𝐫𝐚𝐧𝐭𝐞 𝐢𝐥 𝐫𝐢𝐚𝐯𝐯𝐢𝐨: ${error.message}`)
+        m.reply(`❌ Errore durante il riavvio: ${error.message}`);
     }
 };
 

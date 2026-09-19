6d11b03984fd2d54d72e913f484321f5ab27b8b8
THE PUNISHER <artyom.pennini@gmail.com>
2026-09-19 13:15:05 +0200

    Update fun-tette.js

--- plugins/fun/fun-tette.js
+++ plugins/fun/fun-tette.js
@@ -1,36 +1,26 @@
-//Plugin by Elixir, Punisher & 888 staff
+let handler = async (m, { conn, command, text, args }) => {
 
-
-
-
-
-let handler = async (m, { conn, command, text }) => {
-    
     let target = m.mentionedJid?.[0] 
                 || (args[0]?.includes('@') ? args[0].replace('@', '') + '@s.whatsapp.net' : null)
                 || m.sender
 
     let number = target.split("@")[0]
 
-        let boobsSizes = ['prima', 'seconda', 'terza', 'quarta', 'quinta', 'sesta', 'settima', 'ottava', 'nona', 'decima'];
+    let boobsSizes = ['prima', 'seconda', 'terza', 'quarta', 'quinta', 'sesta', 'settima', 'ottava', 'nona', 'decima'];
 
+    let size = pickRandom(boobsSizes);
 
-        let size = pickRandom(boobsSizes);
+    let boobs = `*🍒 𝐂𝐀𝐋𝐂𝐎𝐋𝐈𝐀𝐌𝐎 𝐋𝐄 𝐓𝐔𝐄 𝐁𝐎𝐎𝐁𝐒 🍒*\n
+━━━━━━━━━━━━━━━━
+*@${number}* ha una *${size}*
+━━━━━━━━━━━━━━━━\n> *888 BOT*`.trim()
 
+    m.reply(boobs, null, { mentions: conn.parseMention(boobs) })
+}
 
-        let boobs = `*🍒 𝐂𝐀𝐋𝐂𝐎𝐋𝐈𝐀𝐌𝐎 𝐋𝐄 𝐓𝐔𝐄 𝐁𝐎𝐎𝐁𝐒 🍒*\n
-    ━━━━━━━━━━━━━━━━
-    *@${number}* ha una *${size}*
-    ━━━━━━━━━━━━━━━━\n> *888 BOT*`.trim()
-
-
-        m.reply(boobs, null, { mentions: conn.parseMention(boobs) })
-    }
-
-
-    function pickRandom(list) {
-        return list[Math.floor(Math.random() * list.length)];
-    }
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
 
 handler.help = ['𝐭𝐞𝐭𝐭𝐞 @𝐭𝐚𝐠']
 handler.tags = ['fun']
